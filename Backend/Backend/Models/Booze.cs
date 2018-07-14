ba94414797d49b1a6a00b15b138f091cc857654d
Douglas Purdy <dpurdy@my.wgu.edu>
2018-07-14 06:52:32 -0400

    Added Booze model, tavern model

---
+++ Backend/Backend/Models/Booze.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Models
+{
+    public class Booze
+    {
+        public string Name { get; set; }
+        public string TypeOfBooze { get; set; }
+    }
+}
---
+++ Backend/Backend/Models/Tavern.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Models
+{
+    public class Tavern
+    {
+        private string Booze;
+        public string Name { get; set; }
+        public string LodgingAvailable { get; set; }
+        public string Location { get; set; }
+        public List<Booze> Boozes { get; set; } = new List<Booze>();
+
+    }
+}
